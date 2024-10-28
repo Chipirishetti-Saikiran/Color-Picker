6f7447a44c594111b8f54b7fa46873afb8d346a1
Chipirishetti Saikiran <109409609+Chipirishetti-Saikiran@users.noreply.github.com>
2024-10-28 22:46:50 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+function c1() {
+    document.getElementById("button1").style.backgroundColor = "#e0e0e0"
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0"
+    document.getElementById("selectedColorHexCode").textContent = "#e0e0e0"
+    document.getElementById("selectedColorHexCode").style.color = "#e0e0e0"
+
+
+}
+
+function c2() {
+    document.getElementById("button2").style.backgroundColor = "#6fcf97"
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97"
+    document.getElementById("selectedColorHexCode").textContent = "#6fcf97"
+    document.getElementById("selectedColorHexCode").style.color = "#6fcf97"
+}
+
+function c3() {
+    document.getElementById("button3").style.backgroundColor = "#56ccf2"
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2"
+    document.getElementById("selectedColorHexCode").textContent = "#56ccf2"
+    document.getElementById("selectedColorHexCode").style.color = "#56ccf2"
+}
+
+function c4() {
+    document.getElementById("button4").style.backgroundColor = "#bb6bd9"
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9"
+    document.getElementById("selectedColorHexCode").textContent = "#bb6bd9"
+    document.getElementById("selectedColorHexCode").style.color = "#bb6bd9"
+}
